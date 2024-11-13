2c2ec34c0d3a37610c24272addb91275d6b18658
fadli <fadlirahmanfauzan04@gmail.com>
2024-11-13 11:07:05 +0700

    learned declaring models & migration

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"baf-credit-score/model"
 	"fmt"
 
 	"gorm.io/driver/postgres"
@@ -9,11 +10,25 @@ import (
 
 func main() {
 	dsn := "host=localhost user=postgres password=123 dbname=baf_credit_score port=5433 sslmode=disable"
-	_,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+	db,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
 	if err != nil {
 		fmt.Printf("Gagal koneksi DB : %v",err)
 	}
 	fmt.Println("Berhasil Koneksi kedalam Database")
+
+	// Auto Migration
+	err = db.AutoMigrate(
+		&model.Customer{}, 
+		&model.User{},
+	)
+
+	// err = db.Migrator().DropColumn(&model.Customer{},"Status") // Untuk menghapus column yang sudah ada
+	if err != nil {
+		fmt.Printf("Gagal Hapus Column : %v",err)
+	}
+	if err != nil {
+		fmt.Printf("Gagal Migration : %v",err)
+	}
 }
 
 
