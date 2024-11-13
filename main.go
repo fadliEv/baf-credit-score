b29216533189c158b88362cccbe718217128e5d2
fadli <fadlirahmanfauzan04@gmail.com>
2024-11-13 13:59:26 +0700

    success create,read, find customer

--- main.go
+++ main.go
@@ -3,40 +3,92 @@ package main
 import (
 	"baf-credit-score/model"
 	"fmt"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var db *gorm.DB // Connection untuk melakukan interaksi dengan ORM (migration,crud, dan lain-lain)
+var errConnection error
+
 func main() {
 	dsn := "host=localhost user=postgres password=123 dbname=baf_credit_score port=5433 sslmode=disable"
-	db,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
-	if err != nil {
-		fmt.Printf("Gagal koneksi DB : %v",err)
+	db, errConnection = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if errConnection != nil {
+		fmt.Printf("Gagal koneksi DB : %v", errConnection)
 	}
 	fmt.Println("Berhasil Koneksi kedalam Database")
 
 	// Auto Migration
-	err = db.AutoMigrate(
-		&model.Customer{}, 
-		&model.User{},
-	)
+	// err := db.AutoMigrate(
+	// 	&model.Customer{},
+	// 	&model.User{},
+	// )
 
 	// err = db.Migrator().DropColumn(&model.Customer{},"Status") // Untuk menghapus column yang sudah ada
+	// if err != nil {
+	// 	fmt.Printf("Gagal Hapus Column : %v",err)
+	// }
+	// if err != nil {
+	// 	fmt.Printf("Gagal Migration : %v",err)
+	// }
+	// registerCustomer()
+	// getAllCustomer()
+
+	// var customers model.Customer
+	// if err := db.Table("customers").First(&customers); err != nil {
+	// 	fmt.Printf("failed get All customer : %v", err.Error.Error())
+	// 	return
+	// }
+	// fmt.Println(customers)
+	// getAllCustomer()
+	findCustomerById()
+}
+
+func registerCustomer() { // Fuction untuk insert data customer kedalam database
+	// sql.exec("inser into...",value,value)
+	birthDateStr := "01-11-2001"
+	format := "02-01-2006"
+	birthDate, err := time.Parse(format,birthDateStr)
 	if err != nil {
-		fmt.Printf("Gagal Hapus Column : %v",err)
+		fmt.Printf("failed parse birthDate : %v", err)
+		return
 	}
-	if err != nil {
-		fmt.Printf("Gagal Migration : %v",err)
+	newCustomer := model.Customer{
+		FullName:    "Juan",
+		PhoneNumber: "098378921184",
+		NIK:         "12378901125",
+		Address:     "Depok",
+		Status:      "active",
+		BirthDate:   birthDate,
+	}
+	errCreate := db.Create(&newCustomer)
+	if errCreate.Error != nil {
+		fmt.Printf("failed insert customer : %v", errCreate.Error)
+		return
+	}
+	fmt.Println("Success register customer")
+}
+
+func getAllCustomer() { // ambil semua record dari table
+	var customers []model.Customer
+	err := db.Find(&customers); 
+	if err.Error != nil {
+		fmt.Printf("failed get All customer : %v", err.Error.Error())
+		return
+	}
+	for _, customer := range customers {
+		fmt.Println(customer)
 	}
 }
 
-
-/**
-01-connection
-02-declare-modes
-03-clean-arch
-..
-unit-testing
-jwt
-**/
+func findCustomerById(){
+	var customer model.Customer
+	err := db.First(&customer,"id = ?","fc973770-fd88-4877-ada8-353687e000b7"); 
+	if err.Error != nil {
+		fmt.Printf("failed get customer by id : %v", err.Error.Error())
+		return
+	}
+	fmt.Println("Customer :", customer)
+}
